ed4fe2a67a66efe7b61c9ae25131b110ad1f0cc2
ivansmarbun-fib <ivan.marbun@futuready.com>
2019-08-03 21:15:38 +0700

    done part 10

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Post
+
 
 def home(request):
     context = {
@@ -9,6 +14,45 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
+
+class PostListViews(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = "posts"
+    ordering = ['-date_posted']
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name_suffix = '_update_form'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog-home')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author 
